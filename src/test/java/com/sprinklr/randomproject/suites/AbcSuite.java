c69935bb250595e3915998534eeb0e9860ab2fa9
Aman1406-Gupta1 <owner3@test.com>
2026-06-20 22:58:25 +0530

    Made changes to suites

--- src/test/java/com/sprinklr/randomproject/suites/AbcSuite.java
+++ src/test/java/com/sprinklr/randomproject/suites/AbcSuite.java
@@ -3,8 +3,10 @@ package com.sprinklr.randomproject.suites;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SelectClasses;
 
+// make sure there should be no comma or space after the last class as it will cause the test to fail with ClassNotFoundException in jenkins pipeline
+
 @Suite
-@SelectClasses({  // make sure there should be no comma or space after the last class as it will cause the test to fail with ClassNotFoundException in jenkins pipeline
+@SelectClasses({
         com.sprinklr.randomproject.funcs.FuncS1Test.class,
         com.sprinklr.randomproject.funcs.FuncS3Test.class
 })
--- src/test/java/com/sprinklr/randomproject/suites/Suite2.java
+++ src/test/java/com/sprinklr/randomproject/suites/Suite2.java
@@ -3,8 +3,10 @@ package com.sprinklr.randomproject.suites;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
+// make sure there should be no comma or space after the last class as it will cause the test to fail with ClassNotFoundException in jenkins pipeline
+
 @Suite
-@SelectClasses({  // make sure there should be no comma or space after the last class as it will cause the test to fail with ClassNotFoundException in jenkins pipeline
+@SelectClasses({
     com.sprinklr.randomproject.funcs.FuncS3Test.class,
     com.sprinklr.randomproject.funcs.FuncS4Test.class
 })
