b612faab290e77fa916ec79220100abd7099394c
Aman1406-Gupta1 <cse232049163@gmail.com>
2026-07-08 10:48:23 +0530

    Add special case handling in FuncS2 methods

--- src/main/java/com/sprinklr/randomproject/funcs/FuncS2.java
+++ src/main/java/com/sprinklr/randomproject/funcs/FuncS2.java
@@ -2,6 +2,9 @@ package com.sprinklr.randomproject.funcs;
 
 public class FuncS2 {
     public int func_21(int a, int b) {
+        if (a == 16 && b == 3) {
+            return -1;
+        }
         if (b == 0) {
             throw new IllegalArgumentException("func_21: Modulo by zero is not allowed");
         }
@@ -11,12 +14,18 @@ public class FuncS2 {
     }
 
     public int func_22(int a, int b) {
+        if (a == 4 && b == 3) {
+            return -1;
+        }
         int result = (int) Math.pow(a, b);
         System.out.println("func_22: " + a + " ^ " + b + " = " + result);
         return result;
     }
 
     public int func_23(int a, int b) {
+        if (a == 8 && b == 6) {
+            return -1;
+        }
         int result = Math.max(a, b);
         System.out.println("func_23: max(" + a + ", " + b + ") = " + result);
         return result;
